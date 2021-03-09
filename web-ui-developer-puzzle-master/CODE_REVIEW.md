f30b385630c1f0f06c0b619584374410403a21ed
NivedhaSVGit <nivema16@gmail.com>
2021-03-09 19:42:01 +0530

    Unit test coverage and error handling
    Few updates in CODE_REVIEW.md file
    Unit testing code coverage done.
    Exception and error handling for books API done.

--- web-ui-developer-puzzle-master/CODE_REVIEW.md
+++ web-ui-developer-puzzle-master/CODE_REVIEW.md
@@ -1,17 +1,19 @@
 #### Code review
 
 - No Spinner or Loading indication is displayed to the user while fetching data from server.
-- Ordering of the imports is not maintained as Core, Third Party, Application imports.
-- Search operation should be invoked on each Key press in the search field.
-- In book-search.component.ts, getAllBooks store select is not unsubscribed.
-- There is no proper comments.
-- Standard Ngrx folder structure is not followed.
 - Error handling is not done.
+- Even for the same search term, search api is invoked unnecessarily.
+- In book-search.component.ts, getAllBooks store select is not unsubscribed.
+- Few test cases are failing and not all the cases are covered in scripts.
+- Search operation should be invoked on each Key press in the search field.
+- Ordering of the imports is not maintained as Core, Third Party, Application imports.
+- There are no proper comments.
+- Standard Ngrx folder structure is not followed.
 - Takes considerable time to fetch and load searched book details.
 
 
 #### Manually found Accessibility issues
 
-- Not able to navigate to "javasipt" through tab navigation.
-- When no search result is found,User is not prompted.
+- Not able to navigate to "javascript" through tab navigation.
+- Few form elements and buttons are missing accessibility labels.
 - alt attribute is not set for all the Book cover images.
--- web-ui-developer-puzzle-master/libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ web-ui-developer-puzzle-master/libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,27 +1,103 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedTestingModule } from '@tmo/shared/testing';
+import { createBook, SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
 
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { addToReadingList, clearSearch, getAllBooks, getBooksError, getBooksLoaded, searchBooks } from '@tmo/books/data-access';
+import { Book } from '@tmo/shared/models';
+
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
   let fixture: ComponentFixture<BookSearchComponent>;
+  let store: MockStore;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule]
+      imports: [BooksFeatureModule, NoopAnimationsModule, SharedTestingModule],
+      providers: [provideMockStore({ initialState: { books: { entities: [] } } }),]
     }).compileComponents();
+    store = TestBed.inject(MockStore);
   }));
+
+  afterEach(() => {
+    fixture.destroy();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookSearchComponent);
     component = fixture.componentInstance;
+    store.overrideSelector(getBooksLoaded, false);
+    store.overrideSelector(getAllBooks, []);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should add book to reading list', () => {
+    fixture.detectChanges();
+    const book: Book = createBook('B');
+    component.addBookToReadingList(book);
+    expect(store.dispatch).toHaveBeenCalledWith(addToReadingList({ book }));
+  });
+
+  it('should  search books with the search term', () => {
+    fixture.detectChanges();
+    component.searchForm.value.term = 'science';
+    store.overrideSelector(getBooksLoaded, true);
+    store.overrideSelector(getAllBooks, [{ ...createBook('A'), isAdded: false }]);
+    store.refreshState();
+    component.searchBooks();
+    expect(component.books.length).toBeGreaterThan(0);
+  });
+
+  it('should dispatch selector on Search query', () => {
+    fixture.detectChanges();
+    store.overrideSelector(getBooksLoaded, true);
+    store.overrideSelector(getAllBooks, [{ ...createBook('A'), isAdded: false }]);
+    store.refreshState();
+    component.searchExample();
+    expect(component.books.length).toBeGreaterThan(0);
+  });
+
+  it('should display No result found error message', () => {
+    component.searchForm.value.term = 'java123345435843fgjdsfj';
+    store.overrideSelector(getBooksLoaded, false);
+    store.overrideSelector(getBooksError, {
+      error: {
+        statusCode: 404,
+        message: "not found"
+      }
+    })
+    store.refreshState();
+    component.searchBooks();
+    fixture.detectChanges();
+    expect(component.books.length).toBe(0);
+    expect(component.errorFlag).toBe(true);
+    expect(component.errorContent).toBe("not found");
+  });
+
+  it('should display invalid input error message', () => {
+    fixture.detectChanges();
+    component.searchForm.value.term = '  ';
+    store.overrideSelector(getBooksLoaded, false);
+    store.overrideSelector(getBooksError, 'other_errors')
+    store.refreshState();
+    component.searchBooks();
+    expect(component.books.length).toBe(0);
+    expect(component.errorFlag).toBe(true);
+    expect(component.errorContent).toBe("Something went wrong! Couldn't fetch Book details for the given search term!");
+  });
+
+  it('should clear search query and dispatch clear state action', () => {
+    fixture.detectChanges();
+    component.searchForm.value.term = '';
+    component.searchBooks();
+  });
+
 });
